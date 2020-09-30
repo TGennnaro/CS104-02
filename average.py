1d837acd47437f28959bcc603dd9770c858deeb5
Tyler Gennaro <71794383+TGennnaro@users.noreply.github.com>
2020-09-30 18:03:14 -0400

    Completed averaging code

--- average.py
+++ average.py
@@ -1,15 +1,15 @@
 #Initialize variables
-total = scoreCount = 0
-average = 0.0
+total = scoreCount = average = 0
 
 #Accept the number of scores to average
 numberOfScores = int(input("Please enter the number of scores you want to average: "))
 
 #Add a loop to make this code repeat until scoreCount = numberOfScores
-score = int(input("Please enter a score: "))
-total += score
-scoreCount += 1
+while scoreCount < numberOfScores:
+    score = int(input("Please enter a score: "))
+    total += score
+    scoreCount += 1
 
-
+#Print the computed average
 average = total / numberOfScores
-print(average)
+print("The average is: "+str(average))
